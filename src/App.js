fcdd3c0e55a835ff3baeb1165f90a512ccac976b
chk512 <kevin.koh@gifs.ca>
2021-06-04 16:33:28 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LinkageGroup from './components/LinkageGroup'
+class App extends Component {
+   render() {
+   return (
+      <div className='App'>
+      <div className='App-header'>
+      <h2>dmap dashboard</h2>
+      </div>
+      <div id='myid'>
+      <LinkageGroup data={[10,20,40,60,80,100,120]} size={[800,500]} />
+      </div>
+      </div>
+   )
+   }
 }
-
-export default App;
+export default App
